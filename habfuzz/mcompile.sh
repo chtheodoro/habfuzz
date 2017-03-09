159f2d10f96cbecedeed4571a2c2b45de6bf8ffd
Christos Theodoropoulos <ctheodor@hcmr.gr>
2017-03-09 18:35:24 +0200

    Delete mcompile.sh

--- habfuzz/mcompile.sh
+++
@@ -1,2 +0,0 @@
-gfortran -c fdeclarations.f95
-gfortran -o habfuzz fdeclarations.f95 habfuzz.f95 hfbayes.f95 rules.f95 fuzzifier.f95 smod.f95 swors.f95 sopt.f95 centroid.f95 maxmem.f95 waver.f95 meanmax.f95
