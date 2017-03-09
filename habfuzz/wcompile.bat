186a641db6fc6b7cb52ab2e324b416bee5daa8df
Christos Theodoropoulos <ctheodor@hcmr.gr>
2017-03-09 18:38:13 +0200

    Delete wcompile.bat

--- habfuzz/wcompile.bat
+++
@@ -1,2 +0,0 @@
-gfortran -c fdeclarations.f95
-gfortran -o habfuzz habfuzz.f95 fdeclarations.f95 hfbayes.f95 rules.f95 fuzzifier.f95 smod.f95 swors.f95 sopt.f95 centroid.f95 maxmem.f95 waver.f95 meanmax.f95
